39070a963e21e5f48e82685c39468786e497fcdd
Peter Georgas <peter_georgas@yahoo.com>
2022-04-28 01:25:04 -0400

    more features

--- lru.go
+++ lru.go
@@ -1,6 +1,9 @@
 package lru
 
-import "container/list"
+import (
+	"container/list"
+	"fmt"
+)
 
 type LRUCache interface {
 	Insert(any)
@@ -9,14 +12,29 @@ type LRUCache interface {
 
 type lruCache struct {
 	Capacity int
-	Mp       map[any]list.Element
+	Mp       map[any]*list.Element
 	List     list.List
 }
 
 func New(capacity int) *lruCache {
-	return &lruCache{Capacity: capacity}
+	return &lruCache{
+		Capacity: capacity,
+		Mp:       make(map[any]*list.Element),
+		List:     *list.New(),
+	}
 }
 
-func (l *lruCache) Insert(any) {
+func (l *lruCache) Insert(v any) {
+	newE := l.List.PushFront(v)
+	l.Mp[v] = newE
+}
 
+func (l *lruCache) Get(v any) {
+	if val, ok := l.Mp[v]; ok {
+		fmt.Println(val.Value)
+		l.List.MoveToFront(val)
+		fmt.Printf("l.List: %v\n", l.List)
+	} else {
+		fmt.Println("VALUE NOT FOUND!")
+	}
 }
--- lru_test.go
+++ lru_test.go
@@ -1,1 +1,13 @@
 package lru_test
+
+import (
+	"lru"
+	"testing"
+)
+
+func TestInsert(t *testing.T) {
+
+	l := lru.New(12)
+	l.Insert(1)
+	l.Get(1)
+}
